91e67530f47f605c4776fbe59e8a66f264d18feb
Kevin Ingersoll <kingersoll@gmail.com>
2012-04-24 04:21:00 -0700

    Return to async

--- src/com/minecarts/dbpermissions/DBPermissions.java
+++ src/com/minecarts/dbpermissions/DBPermissions.java
@@ -187,7 +187,7 @@ public class DBPermissions extends org.bukkit.plugin.java.JavaPlugin implements 
                 calculatePermissions();
             }
             
-        }.sync().fetch(PermissionValue.WILDCARD);
+        }.fetch(PermissionValue.WILDCARD);
     }
     
     
